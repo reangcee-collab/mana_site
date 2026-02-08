3f83707f793fb6f28ba7588667b20132a10a90ea
day six <mattpors@gmail.com>
2026-02-08 13:03:55 +0700

    temp disable manifest staticfiles

--- config/settings.py
+++ config/settings.py
@@ -128,8 +128,8 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
+    "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+},
 }
 
 # Cloudinary config (use Railway Variables)

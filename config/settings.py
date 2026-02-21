dd86eb28be746316d0e3d64a4a9e64a92a06c849
yangkur170 <yangkur7927@gmail.com>
2026-02-22 06:46:50 +0700

    update

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts.apps.AccountsConfig",
+    "django.contrib.humanize",
 ]
 
 # ======================
